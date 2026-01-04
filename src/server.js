26facf4a4e08367636d633c06399b7950e427a79
FlyFayer-byte <krupa.mykhailo@icloud.com>
2026-01-04 20:52:41 +0100

    modified:   .gitignore 	modified:   package.json 	modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
+// src/server/js
+// В цьому файлі знаходиться логіка роботи нашого express-серверу
+
 import 'dotenv/config'; // ініціалізуємо бібліотек для зчитування .env
+
+// Використовуємо значення з .env або дефолтний порт 3000
+const PORT = process.env.PORT ?? 3000;
+
+
+// app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+// });
